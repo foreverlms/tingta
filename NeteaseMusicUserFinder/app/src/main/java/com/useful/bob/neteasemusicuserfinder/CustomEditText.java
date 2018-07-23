53e54ac2aa4434af0f3318af3d4a944c1fb2f5b1
coderchaser <codechaser@613.com>
2018-07-23 14:18:37 +0800

    重新上传

---
+++ NeteaseMusicUserFinder/app/src/main/java/com/useful/bob/neteasemusicuserfinder/CustomEditText.java
@@ -0,0 +1,64 @@
+package com.useful.bob.neteasemusicuserfinder;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+
+/**
+ * Created by bob on 2018/7/23.
+ */
+
+public class CustomEditText extends android.support.v7.widget.AppCompatEditText {
+    private Drawable clearIcon;
+
+    public CustomEditText(Context context){
+        super(context);
+        init();
+    }
+
+    public CustomEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CustomEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        clearIcon = getResources().getDrawable(R.drawable.ic_edittext_clear);
+    }
+
+    @Override
+    protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
+        super.onTextChanged(text, start, lengthBefore, lengthAfter);
+        setClearIconVisible(hasFocus() && text.length() > 0);
+    }
+
+    @Override
+    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
+        super.onFocusChanged(focused, direction, previouslyFocusedRect);
+        setClearIconVisible(focused && length() >0 );
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_UP:
+                Drawable drawable = clearIcon;
+                if (drawable != null && event.getX() <= (getWidth()-getPaddingRight()) && event.getX() >= (getWidth()-getPaddingRight()-drawable.getBounds().width())){
+                    setText("");
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    private void setClearIconVisible(boolean isVisible){
+        setCompoundDrawablesWithIntrinsicBounds(null,null,isVisible ? clearIcon : null,null);
+    }
+}
